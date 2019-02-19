01451bfbee8a9d1d7fd3a6b6c5045602355e2f37
Elijah Hall <ehall851@gmail.com>
2019-02-19 14:36:13 -0700

    i think its counting??

--- simpleClockApp/script.js
+++ simpleClockApp/script.js
@@ -1,23 +1,40 @@
-var date,hour,min,sec,animate;
+var date,h,m,s,animate;
 
 function init() {
     date = new Date();
-    hour = date.getHours();
-    min = date.getMinutes();
-    sec = date.getSeconds();
+    h = date.getHours();
+    m = date.getMinutes();
+    s = date.getSeconds();
+    console.log('working')
 };
 
 function clock() {
-    sec++;
-    if(sec==60){
-        sec=0;
-        min++;
-        if(min==60){
-            min=0;
-            hour++;
-            if(hour==24){
-                hour=0;
+    s++;
+    if(s==60){
+        s=0;
+        m++;
+        if(m==60){
+            m=0;
+            hr++;
+            if(hr==24){
+                hr=0;
             }
         }
     }
+    $('sec','s' );
+    $('min', 'm');
+    $('hr', 'h');
 }
+
+
+animate = setTimeout(clock, 1000);
+
+function $(id,val){
+    if(val < 10){
+        val='0'+val;
+    }
+    document.getElementById(id).innerHTML=val;
+
+}
+
+window.onload=init;
